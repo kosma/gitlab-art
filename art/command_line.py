78829260589e09f75cd30688293c800f393c62b9
Kosma Moczek <kosma@kosma.pl>
2017-01-07 02:24:19 +0100

    art: pep8

--- art/command_line.py
+++ art/command_line.py
@@ -4,24 +4,29 @@ from __future__ import absolute_import
 
 import click
 
+
 @click.group()
 def main():
     """Art, the Gitlab artifact repository client."""
+
 
 @main.command()
 def config():
     """Configure Gitlab URL and access token."""
     click.echo('config')
 
+
 @main.command()
 def update():
     """Update latest tag/branch commits."""
     click.echo('update')
 
+
 @main.command()
 def download():
     """Download artifacts to local cache."""
     click.echo('download')
+
 
 @main.command()
 def install():
