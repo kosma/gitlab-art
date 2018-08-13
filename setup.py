931401af6efc1b6e14f40a8b790979f66f1e459b
Kosma Moczek <kmoczek@fastlogic.pl>
2018-08-13 17:50:26 +0200

    setup.py: bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='art',
-    version='0.1.0',
+    version='0.1.1',
     description='Gitlab artifact manager',
     url='https://github.com/kosma/art',
     author='Kosma Moczek',
