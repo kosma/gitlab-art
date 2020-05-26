83426cf2af539382a0884df58146e48db319f301
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2020-05-25 20:28:13 -0400

    Add README.md contents as package long description

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
+import os
 from setuptools import setup, find_packages
+
+def read_project_file(path):
+    proj_dir = os.path.dirname(__file__)
+    path = os.path.join(proj_dir, path)
+    with open(path, 'r') as f:
+        return f.read()
 
 setup(
     name='gitlab-art',
     version='0.2.0',
     description='Gitlab artifact manager',
+    long_description = read_project_file('README.md'),
+    long_description_content_type = 'text/markdown',
     url='https://github.com/kosma/art',
     author='Kosma Moczek',
     author_email='kosma@kosma.pl',
