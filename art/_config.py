81d971412695ead0c2eb4178d8eeb51ef9396b08
Mike Haboustak <haboustak@gmail.com>
2023-09-17 08:15:59 -0400

    Improve config conversion and validation
    Handle the case where config has both the old and new token settings.
    Also improve error reporting when config is missing (entirely, or any
    required element).

--- art/_config.py
+++ art/_config.py
@@ -25,14 +25,43 @@ def save(gitlab_url, token_type, token):
     _paths.mkdirs(os.path.dirname(_paths.config_file))
     _yaml.save(_paths.config_file, config)
 
-
 def load():
     config = _yaml.load(_paths.config_file)
-   
-    # migrate legacy private_token to token/token_type
-    if 'private_token' in config:
-        config['token'] = config['private_token']
-        config['token_type'] = 'private'
-        del config['private_token']
-        
+
+    if not config:
+        raise click.ClickException('No configuration found. Run "art configure" first.')
+
+    # convert old config to current representation
+    migrate(config)
+
+    validate(config)
+
     return config
+
+def migrate(config):
+    """Perform conversions to maintain backwards-compatibility"""
+
+    # migrate legacy private_token value if it can be
+    # done without overwriting an existing value
+    if 'private_token' in config and 'token' not in config:
+        config['token'] = config['private_token']
+        del config['private_token']
+
+    # default to private tokens if not specified
+    if 'token_type' not in config:
+        config['token_type'] = 'private'
+
+def validate(config):
+    """Ensure the configuration meets expectations"""
+
+    required_fields = ('token', 'token_type', 'gitlab_url')
+    for field in required_fields:
+        if field not in config:
+            raise ConfigException(field, 'Required config element is missing. Run "art configure".')
+
+    # warn the user if they have both private_token and token configured
+    # the private_token element is a legacy field and should be removed
+    if 'private_token' in config and 'token' in config:
+        click.secho('Warning: ', nl=False, fg='yellow')
+        click.echo('Config includes both "token" and "private_token" elements. ', nl=False)
+        click.echo('Only the "token" value will be used.')
